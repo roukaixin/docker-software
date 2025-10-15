23cb64251b1e9d81efecbe95be86d7b4ede6caa4
roukaixin <a3427173515@163.com>
2025-10-15 22:46:09 +0800

    chore: 放开路由规则

--- standalone/sing-box/app/start.sh
+++ standalone/sing-box/app/start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # 添加路由规则
-#ip rule add fwmark 0x1/0x1 lookup 100
-#ip route add local default dev lo table 100
-#
-#ip -6 rule add fwmark 0x1 lookup 106
-#ip -6 route add local ::/0 dev lo table 106
+ip rule add fwmark 0x1/0x1 lookup 100
+ip route add local default dev lo table 100
+
 
 /app/server/run.sh /app/server &
 /app/redirect_pkg_handler &
