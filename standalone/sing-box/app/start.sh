a664406d26516c462f19ec3b9e6961177130f0b3
roukaixin <a3427173515@163.com>
2025-10-17 19:42:44 +0800

    chore: 修改ip规则

--- standalone/sing-box/app/start.sh
+++ standalone/sing-box/app/start.sh
@@ -7,9 +7,17 @@ ip rule add fwmark 1 table 100
 ip route add local 0.0.0.0/0 dev lo table 100
 
 iptables -t mangle -N PROXY
+iptables -t mangle -A PROXY -d 255.255.255.255/32 -j RETURN
+iptables -t mangle -A PROXY -d 192.168.5.0/24 -j RETURN
 iptables -t mangle -A PROXY -p tcp -j TPROXY --on-port 12345 --on-ip 127.0.0.1 --tproxy-mark 1
 iptables -t mangle -A PROXY -p upd -j TPROXY --on-port 12345 --on-ip 127.0.0.1 --tproxy-mark 1
 iptables -t mangle -A PREROUTING -j PROXY
+
+iptables -t mangle -N PROXY_SELF
+iptables -t mangle -A PROXY_SELF -d 255.255.255.255/32 -j RETURN
+iptables -t mangle -A PROXY_SELF -d 192.168.5.0/24 -j RETURN
+iptables -t mangle -A PREROUTING -j MARK --set-mark 1
+iptables -t mangle -A OUTPUT -j PROXY_SELF
 
 
 /app/server/run.sh /app/server &
