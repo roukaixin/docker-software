8e69dfd33a9831b260aa7329e44757b858eeab5e
roukaixin <a3427173515@163.com>
2025-10-17 19:22:47 +0800

    chore: 添加规则

--- standalone/sing-box/app/start.sh
+++ standalone/sing-box/app/start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# 添加路由规则
+apk add --no-cache iptables ca-certificates
+
 ip rule add fwmark 1 table 100
 ip route add local 0.0.0.0/0 dev lo table 100
+
+iptables -t mangle -N PROXY
+iptables -t mangle -A PROXY -p tcp -j TPROXY --on-port 12345 --on-ip 127.0.0.1 --tproxy-mark 1
+iptables -t mangle -A PROXY -p upd -j TPROXY --on-port 12345 --on-ip 127.0.0.1 --tproxy-mark 1
+iptables -t mangle -A PREROUTING -j PROXY
 
 
 /app/server/run.sh /app/server &
