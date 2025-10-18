917ce7beae7e34c6ab66b704f5dcc0a823cf3752
roukaixin <a3427173515@163.com>
2025-10-18 17:47:38 +0800

    chore: 修改命令前后顺序

--- standalone/sing-box/app/server/run.sh
+++ standalone/sing-box/app/server/run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
-apk update
-apk add --no-cache libgcc libelf
-
 sing-box -D "${1}/sing-box" -c "${1}/sing-box/config.json" run &
