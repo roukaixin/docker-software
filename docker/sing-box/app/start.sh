facd4a464e6f325b35e559d53d6002fb1d9f3686
roukaixin <a3427173515@163.com>
2025-12-13 20:33:17 +0800

    chore: 添加 ipv6

--- docker/sing-box/app/start.sh
+++ docker/sing-box/app/start.sh
@@ -7,6 +7,9 @@ apk add --no-cache libgcc libelf
 ip rule add fwmark 0x1/0x1 lookup 100
 ip route add local default dev lo table 100
 
+ip -6 rule add fwmark 0x1 lookup 106
+ip -6 route add local ::/0 dev lo table 106
+
 /app/server/run.sh /app/server &
 /app/redirect_pkg_handler &
 
